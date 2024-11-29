3b66e9cc398db43bc9d4d74e33c08f57fa3903d2
demas <demasrafaat5@gmail.com>
2024-11-29 19:40:41 +0200

    'added'

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /application
+
+COPY print.java .
+
+RUN javac print.java
+
+CMD java print
---
+++ print.java
@@ -0,0 +1,10 @@
+
+package demas.print;
+
+
+public class print {
+
+    public static void main(String[] args) {
+        System.out.println("demas rafaat");
+    }
+}
